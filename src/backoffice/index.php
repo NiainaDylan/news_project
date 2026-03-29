d613be85d9681763a94e783c59a5f19a91f93f4b
Tandrifiniaina Dylan RAJAONARIVONY <tandrifudylan@gmail.com>
2026-03-29 13:25:29 +0300

    ajout route + traitement authentification

--- src/backoffice/index.php
+++ src/backoffice/index.php
@@ -4,3 +4,19 @@ declare(strict_types=1);
 require_once __DIR__ . '/../app/inc/db.php';
 require_once __DIR__ . '/../app/inc/functions.php';
 
+require_once __DIR__ . '/../models/AdminModel.php';
+require_once __DIR__ . '/../controllers/Auth.php';
+
+$action = $_GET['action'] ?? 'home';
+$public = ['login'];
+
+if (!isset($_SESSION['admin']) && !in_array($action, $public)) {
+    redirect('/backoffice/?action=login');
+}
+
+match($action) {
+    'login'     => Auth::login(),
+    'logout'    => Auth::logout(),
+    'home'      => require __DIR__ . '/../views/bo/home.php',
+    default     => redirect('/backoffice/?action=home'),
+};
