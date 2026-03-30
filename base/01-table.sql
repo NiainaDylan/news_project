19e070f89e22f6fda94f943da39a04c0e394be1c
RandriamiarijaonaAlan <randriamiarijaonaalan@gmail.com>
2026-03-30 15:36:34 +0300

    Update 01-table.sql

--- base/01-table.sql
+++ base/01-table.sql
@@ -17,9 +17,13 @@ CREATE TABLE IF NOT EXISTS article (
     id_categorie INT,
     valeur       TEXT,
     date_        TIMESTAMP DEFAULT NOW(),
+    date_cache   TIMESTAMP,
     record_track SMALLINT  DEFAULT 0,
     statut       BOOLEAN   DEFAULT TRUE,
     PRIMARY KEY (id),
     FOREIGN KEY (id_source)    REFERENCES source(id_source),
     FOREIGN KEY (id_categorie) REFERENCES categorie_information(id_categorie)
 );
+
+ALTER TABLE article
+    ADD COLUMN IF NOT EXISTS date_cache TIMESTAMP;
